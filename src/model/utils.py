5de3d3aebd6b29bfd784de5fb325402c8e3e9c14
Riccardo Musmeci <riccardomusmeci@IT000001533447.enelint.global>
2022-10-29 12:23:38 +0200

    documentation

--- src/model/utils.py
+++ src/model/utils.py
@@ -5,16 +5,28 @@ import torch.nn as nn
 
 def create_model(
     model_name: str,
-    pretrained: bool,
+    pretrained: bool = True,
     num_classes: int = 0,
     checkpoint_path: str = None
 ) -> nn.Module:
+    """create model utils function
+
+    Args:
+        model_name (str): model name from timm library
+        pretrained (bool, optional): whether to load pretrained weights. Defaults to True.
+        num_classes (int, optional): num output classes. Defaults to 0.
+        checkpoint_path (str, optional): path to local checkpoint to load. Defaults to None.
+
+    Returns:
+        nn.Module: model
+    """
     
     model: nn.Module = timm.create_model(
         model_name=model_name,
         pretrained=pretrained,
         num_classes=num_classes
     )
+    print(f"> Created {model_name} with {'no ' if not pretrained else ''}pretrained weights. Num classes set to {num_classes}.")
     
     if checkpoint_path is not None:
         assert os.path.exists(checkpoint_path), f"{checkpoint_path} does not exist"
@@ -28,5 +40,4 @@ def create_model(
         model.load_state_dict(state_dict=state_dict)
         print(f"> Loaded state dict from {checkpoint_path}")
     
-    print(f"> Created {model_name} with {'no ' if not pretrained else ''}pretrained weights. Num classes set to {num_classes}.")
     return model
