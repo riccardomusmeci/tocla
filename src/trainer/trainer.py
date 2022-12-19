5bb395a23f451c3adc8c93c5d93dec7f853a7d3a
Riccardo Musmeci <riccardomusmeci@ES000002229902.enelint.global>
2022-12-19 20:00:28 +0100

    added SAM support

---
+++ src/trainer/trainer.py
@@ -0,0 +1,17 @@
+import torch
+
+torch.mps
+
+class Trainer:
+    
+    def __init__(self) -> None:
+        pass
+    
+    def train_epoch(self):
+        pass
+    
+    def val_epoch(self):
+        pass
+    
+    def fit(self):
+        pass
